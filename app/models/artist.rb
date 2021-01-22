27b6b06ad78f6d364855e23108185ca66be70626
baldo-vela <baldo.vela+github@gmail.com>
2021-01-22 15:11:53 -0600

    models updated

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,10 @@
 class Artist < ActiveRecord::Base
+
+    # New relationship
+    has_many :songs
+
+    def song_count
+        #attempt at following the 'one dot' Law of Demeter design principle
+        self.songs.length
+    end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,9 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  #New Method
+  def artist_name
+    self.artist.name
+  end
+  
 end
